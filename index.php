2ff0161c5315bb33fbe432f99bd16f480fb2fb2c
Jeroen De Meerleer <jeroen.de.meerleer@telenet.be>
2014-03-15 18:18:45 +0100

    Initial Commit
    Design, slicing and framework (spoon)

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+/* 
+ * Copyright (C) 2014 Jeroen De Meerleer <me@jeroened.be>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// define location of your library
+define('PATH_LIBRARY', '/home/www/htdocs/instantbugfix.local');
+
+// add this to the include path
+set_include_path(get_include_path() . PATH_SEPARATOR . PATH_LIBRARY);
+
+// require spoon
+require_once 'spoon/spoon.php';
+$site = new init();
+echo $site->working_dir;
+
+class init {
+    
+    public $working_dir;
+    
+    public function __construct() {
+        $working_dir = $this->get_working_directory();
+    }
+    
+    public function get_working_directory() {
+        return $_SERVER['SCRIPT_FILENAME'];
+    }
+    
+}
+
+
